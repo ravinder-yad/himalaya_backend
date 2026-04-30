22ca73efd255ee3a605254acb966a9cc84062a10
Ravinder Yadav <ravinderyadav092007@gmail.com>
2026-04-30 21:25:14 +0530

    chore: add .gitignore and update database configuration

---
+++ config/db.js
@@ -0,0 +1,18 @@
+const mongoose = require("mongoose");
+const { MongoMemoryServer } = require("mongodb-memory-server");
+
+let mongoServer;
+
+module.exports.connect = async () => {
+    mongoServer = await MongoMemoryServer.create();
+    const uri = mongoServer.getUri();
+    await mongoose.connect(uri, {
+        useNewUrlParser: true,
+        useUnifiedTopology: true,
+    });
+};
+
+module.exports.disconnect = async () => {
+    await mongoose.disconnect();
+    await mongoServer.stop();
+};
